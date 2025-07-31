ff51882938376cf60e4fa35016dbe6d8bf1a7da9
mateus <mateusmacedoeng@gmail.com>
2025-07-31 19:01:50 -0300

    updating improvement

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -1,13 +1,10 @@
-import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { View } from 'react-native';
 import { ButtonIcon } from '@/components/ButtonIcon';
 import { Header } from '@/components/Header';
 import { Title } from '@/components/Title';
-import type { StackRoutesList } from '@/routes/StackRoutes';
+import type { StackRoutesProps } from '@/routes/StackRoutes';
 
-type Props = NativeStackScreenProps<StackRoutesList, 'home'>;
-
-export function Home({ navigation }: Props) {
+export function Home({ navigation }: StackRoutesProps<'home'>) {
     return (
         <View style={{ flex: 1, padding: 32, paddingTop: 54 }}>
             <Header>
--- src/app/Product.tsx
+++ src/app/Product.tsx
@@ -1,13 +1,10 @@
-import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { View } from 'react-native';
 import { ButtonIcon } from '@/components/ButtonIcon';
 import { Header } from '@/components/Header';
 import { Title } from '@/components/Title';
-import type { StackRoutesList } from '@/routes/StackRoutes';
+import type { StackRoutesProps } from '@/routes/StackRoutes';
 
-type Props = NativeStackScreenProps<StackRoutesList, 'product'>;
-
-export function Product({ navigation }: Props) {
+export function Product({ navigation }: StackRoutesProps<'product'>) {
     return (
         <View style={{ flex: 1, padding: 32, paddingTop: 54 }}>
             <Header>
