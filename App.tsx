6c2e5b9769e0f2916a2e5325f4a2dc92b58f9472
mateus <mateusmacedoeng@gmail.com>
2025-07-31 17:12:09 -0300

    add product page

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { Home } from '@/app/Home';
+import { Product } from '@/app/Product';
 
 export default function App() {
-    return <Home />;
+    return <Product />;
 }
---
+++ src/app/Product.tsx
@@ -0,0 +1,15 @@
+import { View } from 'react-native';
+import { ButtonIcon } from '@/components/ButtonIcon';
+import { Header } from '@/components/Header';
+import { Title } from '@/components/Title';
+
+export function Product() {
+    return (
+        <View style={{ flex: 1, padding: 32, paddingTop: 54 }}>
+            <Header>
+                <ButtonIcon name="arrow-circle-left" />
+                <Title>Product</Title>
+            </Header>
+        </View>
+    );
+}
