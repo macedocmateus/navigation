b739ab53c61d6f1b6207f75643fc49112d6af371
mateus <mateusmacedoeng@gmail.com>
2025-08-02 19:11:51 -0300

    fix bug

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -3,9 +3,10 @@ import { ButtonIcon } from '@/components/ButtonIcon';
 import { Header } from '@/components/Header';
 import { Title } from '@/components/Title';
 import type { BottomRoutesProps } from '@/routes/BottomRoutes';
+import type { DrawerRoutesProps } from '@/routes/DrawerRoutes';
 import type { StackRoutesProps } from '@/routes/StackRoutes';
 
-export function Home({ navigation }: BottomRoutesProps<'home'>) {
+export function Home({ navigation }: DrawerRoutesProps<'home'>) {
     return (
         <View style={{ flex: 1, padding: 32, paddingTop: 54 }}>
             <Header>
--- src/app/Product.tsx
+++ src/app/Product.tsx
@@ -3,9 +3,10 @@ import { ButtonIcon } from '@/components/ButtonIcon';
 import { Header } from '@/components/Header';
 import { Title } from '@/components/Title';
 import type { BottomRoutesProps } from '@/routes/BottomRoutes';
+import type { DrawerRoutesProps } from '@/routes/DrawerRoutes';
 import type { StackRoutesProps } from '@/routes/StackRoutes';
 
-export function Product({ navigation, route }: BottomRoutesProps<'product'>) {
+export function Product({ navigation, route }: DrawerRoutesProps<'product'>) {
     return (
         <View style={{ flex: 1, padding: 32, paddingTop: 54 }}>
             <Header>
